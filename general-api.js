0a6d36a97dc092397de10410cf5225f41af6494c
Rodriguez Chaparro <diego.rodriguez_chaparro@siemens.com>
2019-04-03 14:32:59 -0500

    Working on JWT Athentication

--- general-api.js
+++ general-api.js
@@ -36,37 +36,49 @@ router.get("/workers", (req, res, err) => {
     console.log("Connected to get")
     var workersQuery;
     con.query("select Especialista.NombreE, Asignacion.CoordenadasEspecialista from asignacion inner join especialista on Especialista.idespecialista = asignacion.idespecialista where idstatus=1 and curdate() between fechainicio and fechafin", (error, result, fields) => {
-        if (error) throw error;
-        workersQuery = result;
-        console.log(workersQuery);
-        res.json(workersQuery);
-    }),     
-    console.log("Done with get")
+            if (error) throw error;
+            workersQuery = result;
+            console.log(workersQuery);
+            res.json(workersQuery);
+        }),
+        console.log("Done with get")
 });
 
 router.get("/allWorkers", (req, res, err) => {
     console.log("Connected to get")
     var workersQuery;
     con.query("select NombreE, IdEspecialista, Foto from Especialista", (error, result, fields) => {
-        if (error) throw error;
-        workersQuery = result;
-        console.log(workersQuery);
-        res.json(workersQuery);
-    }),     
-    console.log("Done with get")
+            if (error) throw error;
+            workersQuery = result;
+            console.log(workersQuery);
+            res.json(workersQuery);
+        }),
+        console.log("Done with get")
 });
 
 
 router.get("/workersList", (req, res, err) => {
     console.log("Connected to get all List")
     var workersQuery;
-    con.query("SELECT Especialista.NombreE, Especialista.Celular, Especialista.IdEspecialista, Tecnica.NombreT, Asignacion.IdAsignacion, Status.NombreS from Especialista inner join Tecnica on Especialista.IdTecnica=Tecnica.IdTecnica inner join Asignacion on Especialista.IdEspecialista = Asignacion.IdEspecialista inner join status on Asignacion.IdStatus=Status.IdStatus WHERE CURDATE() BETWEEN Asignacion.FechaInicio AND Asignacion.FechaFin", (error, result, fields) => {
-        if (error) throw error;
-        workersQuery = result;
-        console.log(workersQuery);
-        res.json(workersQuery);
-    }),     
-    console.log("Done with get all List")
+    con.query("SELECT Especialista.NombreE, Especialista.Celular, Especialista.FechaNacimiento, Especialista.CeCo, Especialista.Foto, Especialista.IdEspecialista, Especialista.GID, Especialista.CedulaCiudadania, Especialista.LugarExpedicion, Especialista.TarjetaIngresoArgos, Especialista.IdTecnica, Tecnica.NombreT,  Asignacion.IdAsignacion, Status.NombreS from Especialista inner join Tecnica on Especialista.IdTecnica=Tecnica.IdTecnica inner join Asignacion on Especialista.IdEspecialista = Asignacion.IdEspecialista inner join status on Asignacion.IdStatus=Status.IdStatus WHERE CURDATE() BETWEEN Asignacion.FechaInicio AND Asignacion.FechaFin", (error, result, fields) => {
+            if (error) throw error;
+            workersQuery = result;
+            for (let worker of workersQuery) {
+
+                function convertBase64(singleWorker) {
+                    if (singleWorker.Foto) {
+                        let pathFoto = singleWorker.Foto;
+                        let bitmap = fs.readFileSync(path.normalize(pathFoto));
+                        return new Buffer(bitmap).toString('base64');
+                    }
+                    return "";
+                }
+                worker["FotoBase64"] = convertBase64(worker);
+            }
+            console.log(workersQuery);
+            res.json(workersQuery);
+        }),
+        console.log("Done with get all List")
 });
 
 router.post("/setAssignment", (req, res, err) => {
@@ -78,40 +90,57 @@ router.post("/setAssignment", (req, res, err) => {
 });
 
 router.get("/deleteWorker/:workerId", (req, res, err) => {
-    if(err) throw err;
+    if (err) throw err;
     console.log("Entered delete");
     console.log(req.params.workerId);
     con.query("delete from Especialista Where IdEspecialista=" + req.params.workerId + ";", (error, result, fields) => {
-        if(error) throw error;
+        if (error) throw error;
         console.log("Borrado ese perro");
-        res.json("se pudo");        
+        res.json("se pudo");
     })
-    // con.query("INSERT INTO Asignacion (IdEspecialista, IdStatus, FechaInicio, FechaFin, CoordenadasSitio, CoordenadasEspecialista, NombreSitio , NombreContacto, TelefonoContacto, Descripcion)", (error, result, fields) =>{
-    //     if(error) throw error;    
-    // })   
     res.json("Done with delete");
 });
 
 router.post("/createWorker", (req, res, err) => {
-    // console.log(req.body);
-    // con.query("INSERT INTO Asignacion (IdEspecialista, IdStatus, FechaInicio, FechaFin, CoordenadasSitio, CoordenadasEspecialista, NombreSitio , NombreContacto, TelefonoContacto, Descripcion)", (error, result, fields) =>{
-    //     if(error) throw error;    
-    // })
-
-    // Saving image to disk
-
-    //if (err) throw err;
     let data = req.body;
 
     // image path
     let base64String = data.Foto;
-    let base64Image = base64String.split(';base64,').pop();   
+    let base64Image = base64String.split(';base64,').pop();
 
     let IdEspecialista = data.IdEspecialista;
     let NombreE = data.NombreE;
     let Celular = data.Celular;
     let IdTecnica = data.IdTecnica;
-    let FechaNacimiento = data.fechaNacimiento;
+    let FechaNacimiento = data.FechaNacimiento;
+    let CeCo = data.CeCo;
+    let GID = data.GID;
+    let CedulaCiudadania = data.CedulaCiudadania;
+    let LugarExpedicion = data.LugarExpedicion;
+    let TarjetaIngresoArgos = data.TarjetaIngresoArgos;
+    let imagePath = variables.serverDirectoryWin + 'images\\\\Foto_' + data.IdEspecialista + ".jpg";
+    console.log(imagePath);
+
+    var query = "INSERT INTO Especialista(IdEspecialista, CeCo, NombreE, TarjetaIngresoArgos, Celular, GID, CedulaCiudadania, LugarExpedicion, FechaNacimiento, IdTecnica, Foto) VALUES(" + IdEspecialista + ",'" + CeCo + "','" + NombreE + "','" + TarjetaIngresoArgos + "','" + Celular + "','" + GID + "','" + CedulaCiudadania + "','" + LugarExpedicion + "','" + FechaNacimiento + "'," + IdTecnica + ",'" + imagePath + "')";
+
+    con.query(query, (error, result, fields) => {
+        if (error) throw error;
+        res.json( saveImage(imagePath, base64Image) ? "Image saved !" : "There was an error with the image" );
+    })
+});
+
+router.post("/editWorker", (req, res, err) => {
+    let data = req.body;
+
+    // image path
+    let base64String = data.Foto;
+    let base64Image = base64String.split(';base64,').pop();
+
+    let IdEspecialista = data.IdEspecialista;
+    let NombreE = data.NombreE;
+    let Celular = data.Celular;
+    let IdTecnica = data.IdTecnica;
+    let FechaNacimiento = data.FechaNacimiento;
     let CeCo = data.CeCo;
     let GID = data.GID;
     let CedulaCiudadania = data.CedulaCiudadania;
@@ -119,23 +148,26 @@ router.post("/createWorker", (req, res, err) => {
     let TarjetaIngresoArgos = data.TarjetaIngresoArgos;
     // Image route
     //let imagePath = path.join(variables.serverDirectoryWin, 'images', data.NombreE + ".jpg"); 
-    let imagePath = variables.serverDirectoryWin + 'images\\\\'+ data.NombreE + ".jpg"; 
+    let imagePath = variables.serverDirectoryWin + 'images\\\\Foto_' + data.IdEspecialista + ".jpg";
     console.log(imagePath);
 
-    var query = "INSERT INTO Especialista(IdEspecialista, CeCo, NombreE, TarjetaIngresoArgos, Celular, GID, CedulaCiudadania, LugarExpedicion, FechaNacimiento, IdTecnica, Foto) VALUES(" + IdEspecialista + ",'" + CeCo + "','" +  NombreE + "','" +  TarjetaIngresoArgos + "','" +  Celular + "','" +  GID + "','" +  CedulaCiudadania + "','" +  LugarExpedicion + "','" +  FechaNacimiento + "'," +  IdTecnica + ",'" + imagePath + "')";
+    var query = "UPDATE Especialista SET NombreE='" + NombreE + "',Celular='" + Celular + "',IdTecnica=" + IdTecnica + ",FechaNacimiento='" + FechaNacimiento + "',CeCo='" + CeCo + "',GID='" + GID + "',CedulaCiudadania='" + CedulaCiudadania + "',LugarExpedicion='" + LugarExpedicion + "',TarjetaIngresoArgos='" + TarjetaIngresoArgos + "',Foto='" + imagePath + "' WHERE IdEspecialista=" + IdEspecialista;
+    console.log(query);
 
-    con.query(query, (error, result, fields) =>{
-        if(error) throw error;
-        fs.writeFile(imagePath, base64Image, {encoding: 'base64'}, (err) => {
-            if (err) console.log('\nError al guardar imagen\n');
-            console.log("\nImage created !!\n");
-        })
-        res.json("Worker created !!");
+    con.query(query, (error, result, fields) => {
+        if (error) throw error;
+        res.json( saveImage(imagePath, base64Image) ? "Image saved !" : "There was an error with the image" );
     })
-
-
-    
 });
+
+
+// Saves the new image
+const saveImage = (imagePath, base64Image) => {
+    fs.writeFile(path.normalize(imagePath), base64Image, { encoding: 'base64' }, (err) => {
+        if (err) return false;
+        return true;
+    })
+};
 
 // router.post('/labs/search', async (req, res) => {
 
@@ -220,4 +252,7 @@ router.post("/createWorker", (req, res, err) => {
 //             })            
 //         })
 // })
-module.exports = { router, startingMysql };
+module.exports = {
+    router,
+    startingMysql
+};
